432e7a922a4d79a4a0b61caebf3fe611e1b3016e
Dale Nguyen <dungnq@itbox4vn.com>
2018-06-03 21:49:41 -0400

    Added security key for requesting data

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -19,8 +19,12 @@ export class Routes {
         .get((req: Request, res: Response, next: NextFunction) => {
             // middleware
             console.log(`Request from: ${req.originalUrl}`);
-            console.log(`Request type: ${req.method}`);
-            next();
+            console.log(`Request type: ${req.method}`);            
+            if(req.query.key !== '78942ef2c1c98bf10fca09c808d718fa3734703e'){
+                res.status(401).send('You shall not pass!');
+            } else {
+                next();
+            }                        
         }, this.contactController.getContacts)        
 
         // POST endpoint
